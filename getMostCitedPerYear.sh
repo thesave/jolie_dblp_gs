0e6a953a8943a02a1f52cc516f5dc87329056f43
thesave <thesave86@gmail.com>
2019-11-04 15:57:34 +0100

    output most cited

--- getMostCitedPerYear.sh
+++ getMostCitedPerYear.sh
@@ -6,5 +6,3 @@ for (( i = 1990; i <= 2019; i++ )); do
   | jq -s -c 'sort_by(.citations | -tonumber)' \
   | jq '.[0:3]' | jq '.[] | "Title : " + .title, "Citations: " + .citations, "Authors: " + if ( .authors.author | type == "string" ) then .authors.author else .authors[] | join(", ") end' 
 done
-
-  # | jq '.[0:3]' | jq -r '.[] | "Title : " + .title, "Citations: " + .citations, "Authors: " + ( if .authors[] | length > 1 then .authors[] | join(",") else .authors[] )'
